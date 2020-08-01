059e96198e0a45851dc18ef0ee254ae33f68e838
maxcrystal <hmi@ya.ru>
2020-08-01 22:55:19 +0300

    simpl-schema client config

--- client/main.jsx
+++ client/main.jsx
@@ -3,6 +3,7 @@ import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
 import { Tracker } from "meteor/tracker";
 
+import "../imports/startup/simpl-schema-configuration";
 import { App } from "/imports/ui/App";
 
 Meteor.startup(() => {
