9ebac2d41da4ca4bb9c242bdfd9e013d0b23d873
maxcrystal <hmi@ya.ru>
2020-08-01 12:03:42 +0300

    add game name check

--- imports/ui/admin/AddGame.jsx
+++ imports/ui/admin/AddGame.jsx
@@ -2,15 +2,17 @@ import React from "react";
 
 const handleOkClick = e => {
   e.preventDefault();
+
   const nameInput = document.getElementById("game-name");
-  Meteor.call(
-    "games.insert",
-    { name: nameInput.value.trim() },
-    (err, newGameId) => {
-      console.log("new game id", newGameId);
-      Meteor.call("couples.assignGame", { gameId: newGameId });
-    }
-  );
+  const newGameName = nameInput.value.trim();
+  if (newGameName.length === 0) {
+    return;
+  }
+
+  Meteor.call("games.insert", { name: newGameName }, (error, newGameId) => {
+    console.log("new game id", newGameId);
+    Meteor.call("couples.assignGame", { gameId: newGameId });
+  });
 
   nameInput.value = "";
 };
