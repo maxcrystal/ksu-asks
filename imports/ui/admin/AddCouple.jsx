42e1f7c4b12b7753bbcb7cf69c3587651c3ac59a
maxcrystal <hmi@ya.ru>
2020-08-02 12:50:44 +0300

    refactoring using useRef

--- imports/ui/admin/AddCouple.jsx
+++ imports/ui/admin/AddCouple.jsx
@@ -1,34 +1,35 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Meteor } from "meteor/meteor";
 
-const handleAddCoupleClick = e => {
-  e.preventDefault();
-  const hisNameInput = document.getElementById("his-name");
-  const herNameInput = document.getElementById("her-name");
-  const he = hisNameInput.value.trim();
-  const she = herNameInput.value.trim();
-  if (he.length === 0 || she.length === 0) {
-    return;
-  }
-
-  Meteor.call("couples.insert", {
-    he,
-    she,
-    gameId: "new-game",
-  });
-  hisNameInput.value = "";
-  herNameInput.value = "";
-};
-
 const AddCouple = ({ game }) => {
+  const hisNameInput = useRef();
+  const herNameInput = useRef();
+
+  const handleAddCoupleClick = e => {
+    e.preventDefault();
+    const he = hisNameInput.current.value.trim();
+    const she = herNameInput.current.value.trim();
+    if (he.length === 0 || she.length === 0) {
+      return;
+    }
+
+    Meteor.call("couples.insert", {
+      he,
+      she,
+      gameId: "new-game",
+    });
+    hisNameInput.current.value = "";
+    herNameInput.current.value = "";
+  };
+
   if (game) {
     return null;
   }
   return (
     <div>
       <form>
-        He: <input type="text" id="his-name" />
-        She: <input type="text" id="her-name" />
+        He: <input type="text" ref={hisNameInput} />
+        She: <input type="text" ref={herNameInput} />
         <button onClick={handleAddCoupleClick}>+</button>
       </form>
     </div>
--- imports/ui/admin/AddGame.jsx
+++ imports/ui/admin/AddGame.jsx
@@ -1,26 +1,27 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useTracker } from "meteor/react-meteor-data";
 
 import { Couples } from "../../api/couples";
 
-const handleOkClick = e => {
-  e.preventDefault();
-
-  const nameInput = document.getElementById("game-name");
-  const newGameName = nameInput.value.trim();
-  if (newGameName.length === 0) {
-    return;
-  }
-
-  Meteor.call("games.insert", { name: newGameName }, (error, newGameId) => {
-    console.log("new game id", newGameId);
-    Meteor.call("couples.assignGame", { gameId: newGameId });
-  });
-
-  nameInput.value = "";
-};
-
 const AddGame = ({ game }) => {
+  const gameNameInput = useRef();
+
+  const handleOkClick = e => {
+    e.preventDefault();
+
+    const newGameName = gameNameInput.current.value.trim();
+    if (newGameName.length === 0) {
+      return;
+    }
+
+    Meteor.call("games.insert", { name: newGameName }, (error, newGameId) => {
+      console.log("new game id", newGameId);
+      Meteor.call("couples.assignGame", { gameId: newGameId });
+    });
+
+    gameNameInput.current.value = "";
+  };
+
   const couples = useTracker(() => {
     return Couples.find({ gameId: "new-game" }).fetch();
   });
@@ -30,7 +31,7 @@ const AddGame = ({ game }) => {
   return (
     <form>
       <span>New game: </span>
-      <input type="text" id="game-name" />
+      <input type="text" ref={gameNameInput} />
       <button onClick={handleOkClick}>Ok</button>
     </form>
   );
