c85dbb953213103265c2a6c81e605c306c39f54d
maxcrystal <hmi@ya.ru>
2020-08-08 10:06:04 +0300

    added go to home when click topbar avatar

--- imports/ui/TopBar.jsx
+++ imports/ui/TopBar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Meteor } from "meteor/meteor";
 import { useTracker } from "meteor/react-meteor-data";
 import { Session } from "meteor/session";
+import { useHistory } from "react-router-dom";
 
 import AppBar from "@material-ui/core/AppBar";
 import ToolBar from "@material-ui/core/Toolbar";
@@ -12,17 +13,18 @@ import Avatar from "@material-ui/core/Avatar";
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles(theme => ({
-  avatar: { margin: theme.spacing(1) },
+  avatar: { margin: theme.spacing(1), cursor: "pointer" },
   title: { flexGrow: 1 },
 }));
 
 const TopBar = () => {
   const classes = useStyles();
+  const history = useHistory();
 
   const serverStatus = useTracker(() => Meteor.status(), []);
   if (serverStatus.staus === "offline") {
     console.log("Client is onffine, reconnecting...");
-    Meteor.reconnect();
+    Meteor.reconnect(); // TODO review how it works
   }
 
   const handleClick = () =>
@@ -36,6 +38,7 @@ const TopBar = () => {
           alt="Ксюша"
           src="/images/ksu.jpg"
           edge="start"
+          onClick={() => history.push("/")}
         />
         <Typography className={classes.title} variant="h6" noWrap>
           Ксюша спрашивает...
